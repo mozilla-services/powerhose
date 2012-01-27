0f5736fbaa910ead12eb51f7d36b974fb5f2f4f5
Tarek Ziade <tarek@ziade.org>
2012-01-27 01:47:23 -0800

    trying more stuff

--- powerhose/master.py
+++ powerhose/master.py
@@ -2,8 +2,7 @@ import time
 import random
 from threading import Thread, RLock
 import contextlib
-from gevent.queue import Queue
-
+from gevent.queue import PriorityQueue, Empty
 import zmq
 
 
@@ -13,13 +12,14 @@ class TimeoutError(Exception):
 
 _ENDPOINT = "ipc://master-routing.ipc"
 _WORKPOINT = "ipc://%s-routing.ipc"
+_WEIGHTS = range(10)
 
 
 class Workers(object):
 
-    def __init__(self, timeout=1.):
+    def __init__(self, timeout=5.):
         self.lock = RLock()
-        self._available = Queue()
+        self._available = PriorityQueue()   # XXX replace with a dict that locks
         self._busy = []
         self.timeout = timeout
         self._workers = []
@@ -33,22 +33,45 @@ class Workers(object):
     @contextlib.contextmanager
     def worker(self):
         _worker = self.acquire()
-        yield _worker
-        self.release(_worker)
+        try:
+            yield _worker
+        finally:
+            self.release(_worker)
 
-    def acquire(self, timeout=1.):
+    def acquire(self, timeout=None):
+        if timeout is None:
+            timeout = self.timeout
+
         # need to catch the empty exception
-        worker = self._available.get(self.timeout)
+        tries = 0
+        worker = None
+
+        while worker is None and worker not in self._workers and tries < 3:
+            try:
+                __, worker = self._available.get(timeout=timeout)
+            except Empty:
+                tries += 1
+                print 'try again'
+
+        if worker is None:
+            raise ValueError()
+
         self._busy.append(worker)
         return worker
 
+    def delete(self, worker):
+        print 'delete a worker ' + worker
+        self._workers.remove(worker)
+
     def add(self, worker):
-        self._available.put(worker)
+        print 'adding a worker ' + worker.identity
+        self._available.put((time.time(), worker))
         self._workers.append(worker.identity)
 
     def release(self, worker):
+        print 'releasing'
         self._busy.remove(worker)
-        self._available.put(worker)
+        self._available.put((time.time(), worker))
 
 
 class WorkerRegistration(Thread):
@@ -81,34 +104,69 @@ class WorkerRegistration(Thread):
             except zmq.ZMQError:
                 break # interrupted
 
+            if events == {}:
+                print 'no event'
+
             for socket in events:
                 msg = socket.recv_multipart()
-                if msg[-2] == 'READY' and msg[-1] not in workers:
-                    name = msg[-1]
-                    # keep track of that worker
-                    print 'adding worker %s' % name
-                    work = self.context.socket(zmq.REQ)
-                    work.connect(name)
-                    work.identity = name
-                    workers.add(work)
+                if msg == ['PING']:
+                    print 'PONG => ' + socket.identity
+                    socket.send('PONG')
+                elif msg[-2] == 'READY':
+                    if msg[-1] in self.workers:
+                        print 'already registered'
+                    else:
+                        name = msg[-1]
+                        # keep track of that worker
+                        print 'adding worker %s' % name
+                        work = self.context.socket(zmq.REQ)
+                        work.connect(name)
+                        work.identity = name
+                        self.workers.add(work)
+                        print 'REGISTERED => ' + socket.identity
+
                     socket.send('REGISTERED')
-                elif msg[-2] == 'REMOVE' and msg[-1] in workers:
-                    delete_worker(msg[-1])
+
+                elif msg[-2] == 'REMOVE':
+                    print '<= REMOVE'
+                    if msg[-1] in self.workers:
+                        workers.delete(msg[-1])
+                        print 'REMOVED => ' + socket.identity
+                    socket.send('REMOVED')
+                elif msg[-2] == 'PING' and msg[-1] in self.workers:
+                    print 'PONG => ' + socket.identity
+                    socket.send('PONG')
+                else:
+                    print 'not implem. ' + str(msg)
 
 
 class JobRunner(object):
-    def __init__(self, workers):
+    def __init__(self, workers, retries=3):
         self.workers = workers
+        self.retries = retries
 
     def execute(self, job_id, job_data, timeout=1.):
+        for i in range(self.retries):
+            try:
+                return self._execute(job_id, job_data, timeout)
+            except TimeoutError:
+                time.sleep(0.1)
+
+        raise TimeoutError()
+
+    def _execute(self, job_id, job_data, timeout=1.):
+        now = time.time()
+
         # errors out if we don't have any worker registered
-        if len(self.workers) == 0:
-            raise ValueError("No Workers!")
-
+        #if len(self.workers) == 0:
+        #    raise ValueError("No Workers!")
         with self.workers.worker() as worker:
-            print 'sending some work at ' + worker.identity
-
-            worker.send("WAKE")  # XXX if this fails we want to try another one
+            print 'WAKE => ' + worker.identity
+            try:
+                worker.send("WAKE", zmq.NOBLOCK)  # XXX if this fails we want to try another one
+            except zmq.ZMQError:
+                print 'wake failed'
+                raise TimeoutError()
 
             poller = zmq.Poller()
             poller.register(worker, zmq.POLLIN)
@@ -117,15 +175,25 @@ class JobRunner(object):
                 try:
                     events = dict(poller.poll(timeout*1000.))
                 except zmq.ZMQError:
+                    print 'interrupted'
                     break # interrupted
+
+                if events == {}:
+                    print 'nothing to see'
+                    if time.time() - now > timeout:
+                        raise TimeoutError()
 
                 for socket in events:
                     msg = socket.recv_multipart()
                     if msg == ['GIVE']:
+                        print 'GIVE <= ' + worker.identity
                         # the worker is ready to get some job done
-                        socket.send_multipart(["JOB", str(job_id), job_data])
+                        print 'JOB => ' + worker.identity
+                        socket.send_multipart(["JOB", str(job_id), job_data],
+                                               zmq.NOBLOCK)
                     elif msg[0] == 'JOBRES':
                         # we got a result
+                        print 'JOBRES <= ' + worker.identity
                         return msg[-1]
                     else:
                         raise NotImplementedError(str(msg))
@@ -140,11 +208,12 @@ if __name__ == '__main__':
     registration.start()
 
     try:
-        while len(workers) < 2:
-            print 'waiting for workers'
-            time.sleep(1.)
+        #while len(workers) < 1:
+        #    print 'waiting for workers'
+        #    time.sleep(1.)
 
-        for x in range(1000):
-            print runner.execute(x, "SOMEDATA")
+        while True:
+            print runner.execute(1, "SOMEDATA")
+            #time.sleep(1.)
     finally:
         registration.stop()
--- powerhose/worker.py
+++ powerhose/worker.py
@@ -2,62 +2,169 @@ import time
 import random
 import sys
 import zmq
+import threading
 
 
 endpoint = "ipc://master-routing.ipc"
 workpoint = "ipc://%s-routing.ipc"
 
+class RegisterError(Exception):
+    pass
 
-def worker(identity):
-    context = zmq.Context()
-    identity = workpoint % identity
 
-    print 'I am worker ' + identity
+class Pinger(threading.Thread):
+    def __init__(self, socket, locker, failed, timeout=1.):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.locker = locker
+        self.running = False
+        self.failed = failed
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
+        self.timeout = timeout
+        self.disabled = False
 
-    # ping the master we are online, with an ID
-    master = context.socket(zmq.REQ)
-    master.connect(endpoint)
-    master.send_multipart(['READY', identity], zmq.NOBLOCK)
-    # XXX need to push this in into a poll
-    # for registration timeout
-    print 'waiting for ack'
-    res = master.recv()
+    def disable(self):
+        self.disabled = True
 
-    # set up the work channel
-    work = context.socket(zmq.REP)
-    work.bind(identity)
+    def enable(self):
+        self.disabled = False
 
-    # setting a poller
-    poller = zmq.Poller()
-    poller.register(work, zmq.POLLIN)
+    def run(self):
+        self.running = True
 
-    now = time.time()
+        while self.running:
+            if self.disabled:
+                time.sleep(1.)
+                continue
 
-    print 'waiting for some work now'
-    while True:
+            with self.locker:
+                try:
+                    self.socket.send('PING', zmq.NOBLOCK)
+                except zmq.ZMQError, e:
+                    print 'could not ping ' + str(e)
+                    self.running = False
+                    break  # interrupted
+
+                try:
+                    events = dict(self.poller.poll(self.timeout))
+                except zmq.ZMQError:
+                    break  # interrupted
+
+                for socket in events:
+                    res = socket.recv()
+                    if res != 'PONG':
+                        print 'ping failed'
+                        self.running = False
+                    else:
+                        print 'master pinged'
+
+            time.sleep(5.)
+
+        self.failed()
+        print 'done'
+
+    def stop(self):
+        if not self.running:
+            return
+        self.running = False
+        self.join()
+
+
+class Worker(object):
+
+    def __init__(self, identity, timeout=1.):
+        self.identity = identity
+        self.ctx = zmq.Context()
+        self.timeout = timeout * 1000
+        self.master = self.ctx.socket(zmq.REQ)
+        self.master.connect(endpoint)
+        self.work = self.ctx.socket(zmq.REP)
+        self.work.bind(identity)
+        self.registered = self.running = False
+        # setting a poller
+        self.poller = zmq.Poller()
+        self.poller.register(self.work, zmq.POLLIN)
+        self.locker = threading.RLock()
+        self.pinger = Pinger(self.master, self.locker, self.failed)
+
+    def failed(self):
+        print 'ping failed lets try to reconnect and die'
+        self._msg('REMOVE', 'REMOVED')
+        self.stop()
+
+    def register(self):
+        self._msg('READY', 'REGISTERED')
+        self.registered = True
+
+    def _msg(self, req, rep):
+        print 'sending to master'
+
+        self.pinger.disable()
         try:
-            events = dict(poller.poll(1000))
-        except zmq.ZMQError:
-            print 'oopsie'
-            break # interrupted
+            print 'locking'
+            with self.locker:
+                poller = zmq.Poller()
+                poller.register(self.master, zmq.POLLIN)
 
-        for socket in events:
-            msg = socket.recv_multipart()
-            print msg
-            if msg == ['WAKE']:
-                # yeah I can work
-                socket.send('GIVE')
-            elif msg[0] == 'JOB':
-                # do the job and send the result
-                print '%s is doing some work' % identity
-                socket.send_multipart(["JOBRES", msg[1], "RESULT"])
-    context.destroy(0)
+                # ping the master we are online, with an ID
+                try:
+                    print '%s => %s' % (req, self.master.identity)
+                    self.master.send_multipart([req, self.identity], zmq.NOBLOCK)
+                except zmq.ZMQError:
+                    raise RegisterError()
+                try:
+                    poller.poll(self.timeout)
+                except zmq.ZMQError:
+                    raise RegisterError()
+
+                res = self.master.recv()
+                if res != rep:
+                    raise RegisterError(res)
+                print '%s <= %s' % (rep, self.master.identity)
+        finally:
+            self.pinger.enable()
+
+    def stop(self):
+        self.running = False
+        self.pinger.stop()
+        time.sleep(.5)
+        self.ctx.destroy(0)
+
+    def run(self):
+        self.running = True
+        self.register()
+        self.pinger.start()
+
+        while self.running:
+            try:
+                events = dict(self.poller.poll(self.timeout))
+            except zmq.ZMQError:
+                break # interrupted
+
+            for socket in events:
+                msg = socket.recv_multipart()
+                print msg
+                if msg == ['WAKE']:
+                    # yeah I can work
+                    socket.send('GIVE')
+                elif msg[0] == 'JOB':
+                    # do the job and send the result
+                    print '%s is doing some work' % self.identity
+                    socket.send_multipart(["JOBRES", msg[1], "RESULT"])
+
+        self._msg('REMOVE', 'REMOVED')
 
 
 if __name__ == '__main__':
+
     try:
-        worker(sys.argv[1])
+        worker = Worker(workpoint % sys.argv[1])
+        worker.run()
+        worker.stop()
+        print 'bye'
     except KeyboardInterrupt:
+        worker.stop()
         print 'bye'
 
 
