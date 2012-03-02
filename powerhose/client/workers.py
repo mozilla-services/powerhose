87571fe2cb2a6d55fad1158196052544ae1bff82
Alexis Metaireau <alexis@notmyidea.org>
2012-03-02 19:18:48 +0100

    pass to circus' get_trainer() the additional keyword arguments passed to Worker

--- powerhose/client/workers.py
+++ powerhose/client/workers.py
@@ -4,9 +4,9 @@ from powerhose import logger
 
 class Workers(object):
     def __init__(self, cmd, num_workers=5, timeout=1.,
-                 check=5., controller='tcp://127.0.0.1:5555'):
+                 check=5., controller='tcp://127.0.0.1:5555', **kw):
         self.trainer = get_trainer(cmd, num_workers, timeout,
-                warmup_delay=check, controller=controller)
+                warmup_delay=check, controller=controller, **kw)
 
     def run(self):
         logger.debug('starting workers')
