a095b86a85cc7c5aa8ae22785a6f96ed354fd26d
Tarek Ziade <tarek@ziade.org>
2012-03-20 14:13:59 +0100

    deactivate the flapping for now

--- powerhose/client/workers.py
+++ powerhose/client/workers.py
@@ -6,7 +6,8 @@ class Workers(object):
     def __init__(self, cmd, num_workers=5, timeout=1.,
                  check=5., controller='tcp://127.0.0.1:5555', **kw):
         self.arbiter = get_arbiter(cmd, num_workers, timeout,
-                                   controller=controller, **kw)
+                                   controller=controller,
+                                   check_flapping=False, **kw)
 
     def run(self):
         logger.debug('starting workers')
