36f86ee5c7d380f858e6bd07c50bcbc93c9761ac
Tarek Ziade <tarek@ziade.org>
2012-04-04 21:43:52 +0200

    one more mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['zmq']
+MOCK_MODULES = ['zmq', 'zmq.eventloop']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
