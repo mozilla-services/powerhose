8a1884ba2d9d4e5efc7becf75fc36634409d6da9
Tarek Ziade <tarek@ziade.org>
2012-04-06 10:16:03 +0200

    using different ipcs per test

--- powerhose/tests/test_cluster.py
+++ powerhose/tests/test_cluster.py
@@ -18,21 +18,23 @@ class TestCluster(unittest.TestCase):
             cl.stop()
 
     def _get_cluster(self, callable):
-        cl = get_cluster(callable, numprocesses=1, background=True)
+        front = 'ipc:///tmp/f-%s' % callable
+        back = 'ipc:///tmp/b-%s' % callable
+        hb = 'ipc:///tmp/h-%s' % callable
+        cl = get_cluster(callable, frontend=front, backend=back, heartbeat=hb,
+                         numprocesses=1, background=True)
         cl.start()
         self.clusters.append(cl)
+        return Client(front)
 
     def test_error(self):
-        self._get_cluster('powerhose.tests.jobs.fail')
-        client = Client()
+        client = self._get_cluster('powerhose.tests.jobs.fail')
         self.assertRaises(ExecutionError, client.execute, 'xx')
 
     def test_timeout(self):
-        self._get_cluster('powerhose.tests.jobs.timeout')
-        client = Client()
+        client = self._get_cluster('powerhose.tests.jobs.timeout')
         self.assertRaises(TimeoutError, client.execute, 'xx')
 
     def test_sucess(self):
-        self._get_cluster('powerhose.tests.jobs.success')
-        client = Client()
+        client = self._get_cluster('powerhose.tests.jobs.success')
         self.assertEqual(client.execute('xx'), 'xx')
