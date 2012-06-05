0b311a4ad3e428e9d4f9a30dd079aa30ca886225
Tarek Ziade <tarek@ziade.org>
2012-06-05 18:52:53 +0200

    starting 0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = ['pyzmq', 'circus']
 
 
 setup(name='powerhose',
-      version='0.5',
+      version='0.6',
       packages=find_packages(),
       include_package_data=True,
       description='Implementation of the Request-Reply Broken pattern in ZMQ',
