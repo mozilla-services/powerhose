6b5c6d5c7e9d8b148398fcba4f24b87c8a5da2ac
Tarek Ziade <tarek@ziade.org>
2012-06-21 16:22:19 +0200

    usign the echo example to bench-test the broker

--- examples/echo_client.py
+++ examples/echo_client.py
@@ -1,36 +1,60 @@
 import random
 import sys
 import time
+import threading
 
-from powerhose.client import Client
+from powerhose.client import Pool
 from powerhose.job import Job
 from powerhose.util import set_logger
 from powerhose.exc import NoWorkerError
 
 
-#set_logger(True)
-client = Client()
 
-#print 'Running 100 echo'
+class Worker(threading.Thread):
 
-start = time.time()
+    def __init__(self, pool):
+        threading.Thread.__init__(self)
+        self.pool = pool
+        self.running = False
 
-#for i in range(2000):
-i = 0
+    def run(self):
+        i = 0
+        self.running = True
 
-while True:
-    data = str(random.randint(1, 1000))
-    job = Job(data)
-    sys.stdout.write(str(i) +  '-> ')
-    sys.stdout.flush()
+        while self.running:
+            data = str(random.randint(1, 1000))
+            job = Job(data)
+            sys.stdout.write(str(i) +  '-> ')
+            sys.stdout.flush()
+            try:
+                res = self.pool.execute(job)
+            except NoWorkerError:
+                sys.stdout.write('NO WORKER\n')
+            else:
+                assert res == data
+                sys.stdout.write('OK\n')
+            sys.stdout.flush()
+            i += 1
+
+    def stop(self):
+        self.running = False
+        self.join()
+
+
+if __name__ == '__main__':
+    # a pool of 10 workers
+    client = Pool()
+
+    # 10 threads hammering the broker
+    workers = [Worker(client) for i in range(20)]
+    for worker in workers:
+        worker.start()
+
+    # just sit there for 100 seconds
     try:
-        res = client.execute(job)
-    except NoWorkerError:
-        sys.stdout.write('NO WORKER\n')
-    else:
-        assert res == data
-        sys.stdout.write('OK\n')
-    sys.stdout.flush()
-    i += 1
+        time.sleep(3600)
+    finally:
+        for worker in workers:
+            worker.stop()
 
-print 'Done in %.2f' % (time.time() - start)
+
