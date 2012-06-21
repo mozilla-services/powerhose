7847a07cb83e86879634ed2ad02227488c0d0ec2
Tarek Ziade <tarek@ziade.org>
2012-06-21 16:53:44 +0200

    starting 0.8

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = ['pyzmq', 'circus']
 
 
 setup(name='powerhose',
-      version='0.7',
+      version='0.8',
       packages=find_packages(),
       include_package_data=True,
       description='Implementation of the Request-Reply Broker pattern in ZMQ',
